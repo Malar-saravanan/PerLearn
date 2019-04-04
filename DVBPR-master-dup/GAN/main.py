f470597a262078cf68d4d8a1e9e2fbe41cfbc0bc
Malar Saravanan <malarvizhi.saravanan@siriuscom.com>
2019-04-04 13:15:31 +0530

    main.py in GAN

--- DVBPR-master-dup/GAN/main.py
+++ DVBPR-master-dup/GAN/main.py
@@ -17,7 +17,7 @@ flags.DEFINE_integer("input_height", 64, "The size of image to use (will be cent
 flags.DEFINE_integer("input_width", None, "The size of image to use (will be center cropped). If None, same value as input_height [None]")
 flags.DEFINE_integer("output_height", 64, "The size of the output images to produce [64]")
 flags.DEFINE_integer("output_width", None, "The size of the output images to produce. If None, same value as output_height [None]")
-flags.DEFINE_string("dataset", "AmazonFashion6ImgPartitioned.npy", "The name of dataset [celebA, mnist, lsun]")
+flags.DEFINE_string("dataset", "/content/PerLearn/DVBPR-master-dup/AmazonFashion6ImgPartitioned.npy", "The name of dataset [celebA, mnist, lsun]")
 flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
